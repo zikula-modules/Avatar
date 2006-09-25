6167d3e4a366964dd40798483e3c3400b025646c
jna <jna@code.zikula.org>
2006-09-25 19:07:44 +0000

    updated version number
    git-svn-id: file:///home/trac/svn/avatar/trunk@16 5472382f-102b-0410-a446-e262bff5f5a1

--- Avatar/modules/Avatar/pnversion.php
+++ Avatar/modules/Avatar/pnversion.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 /**
  * Avatar Module
- * 
+ *
  * The Avatar module allows uploading of individual Avatars.
  * It is based on EnvoAvatar from A.T.Web, http://www.atw.it
  *
@@ -14,15 +14,15 @@
  */
 
 $modversion['name']           = 'Avatar';
-$modversion['version']        = '1.0';
+$modversion['version']        = '1.1';
 $modversion['description']    = 'Upload of individual Avatars';
-$modversion['credits']        = 'docs/credits.txt';
-$modversion['help']           = 'docs/installa.txt';
-$modversion['changelog']      = 'docs/changelog.txt';
-$modversion['license']        = 'docs/license.txt';
+//$modversion['credits']        = 'docs/credits.txt';
+//$modversion['help']           = 'docs/installa.txt';
+//$modversion['changelog']      = 'docs/changelog.txt';
+//$modversion['license']        = 'docs/license.txt';
 $modversion['official']       = 0;
 $modversion['author']         = 'Joerg Napp';
-$modversion['contact']        = 'http://lottasophie.sf.net';
+$modversion['contact']        = 'http://noc.postnuke.com/projects/avatar/';
 $modversion['admin']          = 1;
 $modversion['securityschema'] = array('Avatar::' => '::');
 
