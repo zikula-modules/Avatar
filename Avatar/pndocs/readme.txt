ad83d05baf53ef873e4d409242740e4e39592013
Carsten Volmer <carsten@zikula.org>
2010-04-22 07:27:47 +0000

    Minor updates (incl. docblocks). Finally closes #23 and #28
    git-svn-id: file:///home/trac/svn/avatar/trunk@133 5472382f-102b-0410-a446-e262bff5f5a1

--- Avatar/pndocs/readme.txt
+++ Avatar/pndocs/readme.txt
@@ -1,14 +1,11 @@
 /**
  * Avatar Module
  * 
- * The Avatar module allows uploading of individual Avatars.
- * It is based on EnvoAvatar from A.T.Web, http://www.atw.it
- * 
  * @package      Avatar
  * @version      $Id$
- * @author       Joerg Napp, Frank Schummertz, Carsten Volmer
- * @link         http://lottasophie.sf.net, http://www.pn-cms.de
- * @copyright    Copyright (C) 2004-2007
+ * @author       Joerg Napp, Frank Schummertz, Carsten Volmer, Carsten Volmer
+ * @link         http://code.zikula.org/avatar
+ * @copyright    Copyright (C) 2004-2010
  * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
  */
  
--- Avatar/pnuser.php
+++ Avatar/pnuser.php
@@ -2,14 +2,11 @@
 /**
  * Avatar Module
  *
- * The Avatar module allows uploading of individual Avatars.
- * It is based on EnvoAvatar from A.T.Web, http://www.atw.it
- *
  * @package      Avatar
  * @version      $Id$
- * @author       Joerg Napp, Frank Schummertz
- * @link         http://lottasophie.sf.net, http://www.pn-cms.de
- * @copyright    Copyright (C) 2004-2007
+ * @author       Joerg Napp, Frank Schummertz, Carsten Volmer
+ * @link         http://code.zikula.org/avatar
+ * @copyright    Copyright (C) 2004-2010
  * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
  */
 
@@ -38,6 +35,19 @@ function Avatar_user_main()
     if (empty($profileModule) || !pnModAvailable($profileModule)) {
         LogUtil::registerError(__('Error! This module can only be used if the Profile module is installed and if it is set to the default profile module.', $dom));
         return pnRedirect(pnConfigGetVar('entrypoint', 'index.php'));
+    }
+
+    pnModDBInfoLoad('Profile', 'Profile');
+    $dudfields = DBUtil::selectObjectArray('user_property');
+    $propcheck = false;
+    foreach($dudfields as $dudfield) {
+        if ($dudfield['prop_attribute_name'] == 'avatar') {
+            $propcheck = true;
+        }
+    }
+
+    if ($propcheck == false || empty($dudfields))  {
+        return LogUtil::registerError(__('Error! The user attribute called \'avatar\' doesn\'t exist. Please install a Profile module if you want to use this module.', $dom));
     }
 
     // get all possible avatars
